61dab6e2e138e5e6c08fd36d20465ae5d056c7a7
Marlene Oteiza <marleneoteiza@outlook.com>
2022-11-06 02:33:11 -0300

    Cambios

---
+++ javascript/index.js
@@ -0,0 +1,45 @@
+/*let nombre = "Marlene";
+let apellido = "Oteiza";
+let edad = 30;
+let mascota = "no tengo";
+
+console.log(typeof nombre)
+console.log(nombre)
+
+console.log(typeof apellido)
+console.log(apellido)
+
+console.log(typeof edad)
+console.log(edad)
+
+console.log(typeof mascota)
+console.log(mascota) */
+
+/*let nombreIngresado = prompt("Ingrese su nombre")
+
+alert("¡Hola Mundo!")*/
+
+/*let entrada = prompt("Ingrese su nombre");
+let salida = (entrada + " " + "ingresada");
+
+alert(salida)
+
+
+
+let numeroUno = 5;
+
+let numeroDos = 10;
+
+let resultadoSuma = numeroUno + numeroDos
+
+console.log(resultadoSuma)
+
+let entryNumber = prompt("Ingrese un valor númerico");
+
+if (a > 5) {
+    console.log("El número es mayor a 5")
+}*/
+
+let
+
+
